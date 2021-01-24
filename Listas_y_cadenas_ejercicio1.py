1abddc54298c83d752a70ea2ba525e7dbe71df3a
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2021-01-23 18:51:27 -0600

    Add files via upload

---
+++ Listas_y_cadenas_ejercicio1.py
@@ -0,0 +1,11 @@
+#Jose Pablo Ponce, 19092
+#13-02-19
+#listas_y_cadenas_1
+#Crear un programa que al ingresar la placa de un carro me muestre la posicion en la que se encuentra parqueado
+
+placa= input('ingrese placa')
+
+datos =['P-789FDS','P-349USK','P875KJS','P-156UER','P-452LDM','P-629BWP']
+datos2 =['A1','A2','B1','B2','C1','C2']
+
+print (datos2[datos.index(placa)])
---
+++ listas_y_cadenas_ejercicio2.py
@@ -0,0 +1,9 @@
+#Jose Pablo Ponce, 19092
+#13-02-19
+#listas_y_cadenas_2
+#Crear un programa donde haga una lista de contactos y ordenarla
+nombre = input ('ingrese los nombres y apellidos seguidos de una ,' )
+
+nombre2= nombre.split(', ')
+
+print(nombre2)
